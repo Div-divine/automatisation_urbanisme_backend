54e2cdbaa574a754dd271ce3b40e59050af56645
divine <divineosuuweb@gmail.com>
2024-08-07 15:50:56 +0200

    First commit with testing route to ensure server connection

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Body parser (configure it before defining routes)
+app.use(bodyParser.json());
+
+// Import routes
+import indexRouter from './routes/indexRouter.js';
+
+// Define routes
+app.use('/', indexRouter);
+
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
